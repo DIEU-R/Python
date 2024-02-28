917a6f0080cc07d76a448aba4242da98a591d5fb
Robert Merchant <robertmerchant10927219@gmail.com>
2024-02-28 13:40:36 +0530

    Match Case --Max Function

--- Interview/Max.py
+++ Interview/Max.py
@@ -1,7 +1,11 @@
+from pickle import NONE
+from uu import Error
+
+
 li=[1,2,3,34,23,564,6,546,234,4,5,6]
-def Max(list :list[int])->int:
+def Max(list :list[int])->int|None:
     match list:
-        case []: raise ValueError("Plese Enter number ! list is Empty!!!")
+        case []: print("Plese Enter number ! list is Empty!!!") 
         case [x]: return x
         case [x,*x_rest] :return max(x,Max(x_rest))
 
