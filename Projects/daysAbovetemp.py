42e7f31afa371084282b639fb6d8f6ad2079a2c6
Robert Merchant <robertmerchant10927219@gmail.com>
2024-02-29 02:21:15 +0530

    number of days having temp above Avg

---
+++ Projects/daysAbovetemp.py
@@ -0,0 +1,19 @@
+import statistics
+
+def DAT2():
+    tem=[]
+    d_day=0
+    days=int(input("Enter no of days : "))
+    for i in range(1,days+1):
+        temp = int(input("Enter day "+str(i)+"'s Temperature : "))
+        tem.append(temp)
+    Avg=round(statistics.mean(tem),2)
+    print("Average Temperature : "+ str(Avg))
+    
+    for i in tem:
+        if i>Avg:
+            d_day += 1
+    print("Number of Days having Temperature above Average "+ str(d_day))
+    
+
+DAT2()
