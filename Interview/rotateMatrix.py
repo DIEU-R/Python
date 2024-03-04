1acae7c178eb10dc96daedc2e3a1878ac6be00c4
Robert Merchant <robertmerchant10927219@gmail.com>
2024-03-04 19:04:11 +0530

    checking for 4x4

--- Interview/rotateMatrix.py
+++ Interview/rotateMatrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-my_array=np.array([[1,2,3],[4,5,6],[7,8,9]])
+my_array=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 print(my_array)
 def rotatematrix(matrix):
     n=len(matrix)
