507644c81ad05f606b50f03e086b97211d8a0418
Robert Merchant <robertmerchant10927219@gmail.com>
2024-03-04 23:32:59 +0530

    Method -3 Fibonacci

--- Interview/Fibonacci.py
+++ Interview/Fibonacci.py
@@ -1,15 +1,6 @@
-
-def Fib(num):
-    if (num==0 or num==1):
-        return num
-    return Fib(num-1) + Fib(num-2)
-
-
-print(Fib(9))
-
-def allFib(n):
+def Fibonacci1(n):
     for i in range(n):
-        print(str(i) +":, "+ str(fib(i)))
+        print(str(i+1) +":, "+ str(fib(i)))
 
 
 def fib(n):
@@ -19,9 +10,19 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
-print(allFib(5))
+print(Fibonacci1(5))
 
 def Fibonacci(num :int) -> int:
     match num:
         case 0 | 1:return num 
         case _ : return Fibonacci(num-1) + Fibonacci(num-2)
+
+
+def Fibonacci2(num):
+    a,b=0,1
+    for i in range(num):
+        yield a
+        a,b=b,a+b
+
+for n in Fibonacci2(10):
+    print(n)
