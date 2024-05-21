bb6f744edc876c0ba4862e79c9314ea95a530ce4
Robert Merchant <robertmerchant10927219@gmail.com>
2024-05-22 00:40:18 +0530

    Added Insert Value option

--- LinkedList/CircularSinglyLinkedList.py
+++ LinkedList/CircularSinglyLinkedList.py
@@ -25,8 +25,36 @@ class CircularSinglyLinkedList:
         self.tail = node
         return "The Circular Singly Linked List has been created"
     
-circularSLL = CircularSinglyLinkedList()
-circularSLL.createCSLL(20)
 
+    def insertCSLL(self,value,location):
+        if self.head is None:
+            return "The head reference is None"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+            elif location ==1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index <location-1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode             
+            return "The node has been successfully inserted in the Circular Singly Linked List"
+
+
+circularSLL = CircularSinglyLinkedList()
+print(circularSLL.createCSLL(10))
+circularSLL.insertCSLL(101,0)
+circularSLL.insertCSLL(102,1)
+circularSLL.insertCSLL(1003,2)
 
 print([node.value for node in circularSLL])
