69eda789a3bb252d565c97f0d6166c80df15fe3a
Robert Merchant <robertmerchant10927219@gmail.com>
2024-05-23 23:50:27 +0530

    Insert Node function has been created

--- LinkedList/DoubleLinkedList.py
+++ LinkedList/DoubleLinkedList.py
@@ -1,8 +1,5 @@
-from tkinter import ANCHOR
-
-
 class Node:
-    def __init__(self, value=None):
+    def __init__(self, value):
         self.value= value
         self.next= None
         self.prev= None
@@ -28,8 +25,44 @@ class DoublyLinkedList:
         self.tail = node
         return "The DLL has been created"
 
+    def insertNode(self, value,location):
+        if self.head is None:
+            return "The DLL does not exist"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                newNode.prev = None
+                self.head.prev = newNode
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index <location -1:
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+            return "The node has been successfully inserted"
+
+
 
 
 doublyLL= DoublyLinkedList()
-doublyLL.createDLL("10")
-print([node.value for node in doublyLL])    
+doublyLL.createDLL(10)
+print([node.value for node in doublyLL])
+
+doublyLL.insertNode(20,0)
+doublyLL.insertNode(30,1)
+doublyLL.insertNode(40,2)
+doublyLL.insertNode(50,3)
+doublyLL.insertNode(60,4)
+doublyLL.insertNode(70,5)
+print([node.value for node in doublyLL])
