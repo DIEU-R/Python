f67cc03100fc1a3293e2259fbdb635bba3245eea
Robert Merchant <robertmerchant10927219@gmail.com>
2024-05-27 00:12:29 +0530

    modified:   CircularDoubleLinkedList.py

--- LinkedList/CircularDoubleLinkedList.py
+++ LinkedList/CircularDoubleLinkedList.py
@@ -27,9 +27,44 @@ class CircularDoubleLinkedList:
         newNode.next = newNode
         return "The Circular Double Linked List has been created"
     
+    def insertCDLL(self, value, location):
+        if self.head is None:
+            return " The CDLL does not exist"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.head = newNode
+                self.tail.next = newNode
+            elif location == -1:
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while location <index:
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+            return "The node has been successfully inserted in the Circular Double Linked List"
+
 
 
 
 circularDLL = CircularDoubleLinkedList()
 print(circularDLL.createCDLL(11))
+print([node.value for node in circularDLL])
+
+circularDLL.insertCDLL(0,0)
+circularDLL.insertCDLL(1,1)
+circularDLL.insertCDLL(2,1)
+circularDLL.insertCDLL(3,1)
+circularDLL.insertCDLL(4,-1)
 print([node.value for node in circularDLL])
