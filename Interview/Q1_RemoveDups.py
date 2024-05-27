f280d5fe0dccccae70568041caac8bfcbf949e16
Robert Merchant <robertmerchant10927219@gmail.com>
2024-05-28 02:27:11 +0530

    Remove Duplicate Meth-2

--- Interview/Q1_RemoveDups.py
+++ Interview/Q1_RemoveDups.py
@@ -15,9 +15,26 @@ def removeDups(LL):
                 curNode = curNode.next
         return LL
     
+def removeDups1(LL):
+    if LL.head is None:
+        return 
+    else:
+        curNode = LL.head
+        while curNode:
+            runner = curNode
+            while runner.next:
+                if runner.next.value == curNode.value:
+                    runner.next = runner.next.next
+                else:
+                    runner =runner.next
+            curNode = curNode.next
+        return LL
+    
 
 customLL = LinkedList()
 customLL.generate(10,0,99)
 print(customLL)
 removeDups(customLL)
+print(customLL)
+removeDups1(customLL)
 print(customLL)
